f0f06a82061cac9be56e323c24da002435870319
Dingqinliu <dingqinliu@163.com>
2021-07-27 21:31:23 +0800

    用户model

---
+++ User.java
@@ -0,0 +1,8 @@
+package com.dingqinliu.model;
+
+public class User {
+    public Integer uid; //用包装类型Integer而不用 int 是因为 区分null和数字的区别 合理性
+    public String  username;
+    public String nickname;
+
+}
