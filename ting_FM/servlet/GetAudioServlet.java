a4ebecd5f4b2bef2389d1de9a47b6c0924a9048b
Dingqinliu <dingqinliu@163.com>
2021-08-03 15:56:32 +0800

    交互-声音输出

--- ting_FM/servlet/GetAudioServlet.java
+++ ting_FM/servlet/GetAudioServlet.java
@@ -3,11 +3,14 @@ package com.dingqinliu.servlet;
 import com.dingqinliu.service.StoryService;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 
 @WebServlet("/get-audio")
 public class GetAudioServlet extends HttpServlet {
@@ -22,6 +25,29 @@ public class GetAudioServlet extends HttpServlet {
         }
 
         int sidInt=Integer.parseInt(sid);
-        storyService.getAudio(sidInt);
+        try {
+            InputStream is = storyService.getAudio(sidInt);
+            if (is==null){
+                resp.sendError(404,"sid对应的声音不存在");
+                return;
+            }
+            //进行声音输出：按照二进制方式把声音写入resp的输出流中
+            ServletOutputStream os = resp.getOutputStream();
+            byte[] buf=new byte[1024];
+            int len;
+
+            while (true){
+                len=is.read(buf);
+                if (len==1){
+                    break;
+                }
+
+                os.write(buf,0,len);
+            }
+            os.flush();
+            is.close();
+        }catch (SQLException exc){
+            throw new ServletException(exc);
+        }
     }
 }
